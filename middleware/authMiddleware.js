8cbed0cdc5bc7cdaabe4e0274749064729d6e73d
Laksmartt <lakshaymarwaha0104@gmail.com>
2025-05-26 23:52:48 -0400

    final commit

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,16 +1,23 @@
 const jwt = require('jsonwebtoken');
+const {getUserByUsername} = require("../models/userModel")
 
-module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  const token = authHeader && authHeader.split(' ')[1];
+module.exports = async (req, res, next) => {
+  const token = req.cookies["authCookie"];
 
-  if (!token) return res.status(401).json({ message: 'Missing token' });
+  if (!token) return res.status(401).json({ message: "Missing token" });
 
-  try {
-    const decoded = jwt.verify(token, 'secretkey');
-    req.user = decoded;
-    next();
-  } catch {
-    res.status(401).json({ message: 'Invalid token' });
-  }
+  jwt.verify(token, process.env.TOKEN_SECRET, async (err, payload) => {
+    if (err) return res.status(403).json({ message: "Invalid token" });
+
+    try {
+      const user = await getUserByUsername(payload.username);
+      if (!user) return res.status(403).json({ message: "User not found" });
+
+      req.user = { id: user.user_id, username: user.username, role: user.role };
+      next();
+    } catch (error) {
+      return res.status(500).json({ message: "Internal server error" });
+    }
+  });
 };
+
